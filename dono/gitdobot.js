09f3ba6d66379e52db1c98bc729b6c14af05cc51
seunome <you@example.com>
2022-12-06 00:22:22 -0300

    First commit

---
+++ dono/gitdobot.js
@@ -0,0 +1,7 @@
+const gitdobot = (prefix) => {
+return`
+*SÓ NO CANAL DO MEU CRIADOR ^-^ LÁ TEM O VÍDEO, O LINK DO BOT TA NOS COMENTÁRIOS FIXADOS :) https://youtube.com/channel/UCPEVqKFH45x4grF2qk3fldw*
+`
+}
+
+exports.gitdobot = gitdobot
